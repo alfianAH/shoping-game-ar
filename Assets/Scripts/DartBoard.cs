93655c86128aab4e9b6271bf4784f88dc39d7a39
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-08 18:01:17 +0800

    Make dart board

---
+++ Assets/Scripts/DartBoard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartBoard : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+    }
+    
+}
