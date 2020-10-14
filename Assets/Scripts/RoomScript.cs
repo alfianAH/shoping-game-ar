dc98ee9cc24db17f3bb55054213f641d678a8d37
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-14 17:29:58 +0800

    Make room so dart can be stop from falling

---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Dart"))
+        {
+            Rigidbody dartRigidbody = other.GetComponent<Rigidbody>();
+            Dart dart = other.GetComponent<Dart>();
+            
+            // Stop dart
+            dartRigidbody.useGravity = false;
+            dartRigidbody.isKinematic = true;
+            dart.IsThrowing = false;
+            dart.IsNotUsed = true;
+        }
+    }
+}
